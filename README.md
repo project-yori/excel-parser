4b20e48b706c2d6644023300a778e790b5e19a8a
Asuka Wang <asukachikaru@gmail.com>
2019-08-09 17:11:25 +0900

    Remove hardcoded filename, auto read files in ./filesToParse

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # excel-parser
 This is a excel-json parser built with python
+
+## Usage
+- `$ pip -r requirements.txt`
+- **NECESSARY** Put files to parse in folder `filesTofolder`
+- `$ python3 main.py`
+- Output json will be in folder `output/`
--- main.py
+++ main.py
@@ -1,32 +1,49 @@
 from openpyxl import load_workbook
 import json
+import os
 
-wb = load_workbook('K46.xlsx')
-ws = wb['keyakizaka46']
+# Get paths of files to parse
+path = './filesToParse'
+filesToParse = []
 
-members = []
+for files in os.walk(path):
+  for file in files[2]:
+    if('.xlsx' in file):
+      filesToParse.append('{}/{}'.format(path, file))
 
-# Read properties
-props = []
-for column in range(1, 8):
-  props.append(ws.cell(row=1, column=column).value)
+if(not os.path.exists('./output')):
+  os.mkdir('output')
 
-# Count number of rows
-numOfRow = 1
-while(ws.cell(row=numOfRow, column=1).value != None):
-  numOfRow += 1
+def parse(filesToParse):
+  for file in filesToParse:
+    wb = load_workbook(file)
+    ws = wb[wb.sheetnames[0]]
+        
+    members = []
 
-# Iterate cells and save into array
-for row in range(2, numOfRow):
-  newMember = {}
-  for column in range(0, 7):
-    data = ws.cell(row=row, column=column+1).value
-    if(props[column]=='graduated'):
-      data = True if data == 1 else False
-    newMember[props[column]] = data
-  members.append(newMember)
+    # Read properties
+    props = []
+    for column in range(1, 8):
+      props.append(ws.cell(row=1, column=column).value)
 
-# Write to json
-with open('members.json', 'w') as output:
-  json.dump(members, output, ensure_ascii=False);
+    # Count number of rows
+    numOfRow = 1
+    while(ws.cell(row=numOfRow, column=1).value != None):
+      numOfRow += 1
+
+    # Iterate cells and save into array
+    for row in range(2, numOfRow):
+      newMember = {}
+      for column in range(0, 7):
+        data = ws.cell(row=row, column=column+1).value
+        if(props[column]=='graduated'):
+          data = True if data == 1 else False
+        newMember[props[column]] = data
+      members.append(newMember)
+
+    # Write to json
+    fileName = file.replace('.xlsx', '').replace(path+'/', '');
+    with open('output/{}.json'.format(fileName), 'w') as output:
+      json.dump(members, output, ensure_ascii=False);
     
+parse(filesToParse)
